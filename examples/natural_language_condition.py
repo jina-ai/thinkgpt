b25c269102e66190db144d7f3c43075a0ca33de2
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-04-16 17:15:51 +0100

    natural language condition example

---
+++ examples/natural_language_condition.py
@@ -0,0 +1,11 @@
+from examples.knowledge_base import knowledge
+from llm_tools.llm import ThinkGPT
+
+llm = ThinkGPT(model_name="gpt-3.5-turbo")
+
+task = 'generate python code that uses langchain to send a request to OpenAI LLM and ask it suggest 3 recipes using gpt-4 model'
+if not llm.condition(f'Do you know all the requirements to achieve the following task ? {task}'):
+    print('learning knowledge')
+    llm.memorize(knowledge)
+
+print(llm.predict(task, remember=llm.remember(task, sort_by_order=True, limit=5)))
