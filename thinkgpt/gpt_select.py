2cb3d6478e82a50d5e61ce563902ac33bb192e92
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-04-21 01:34:19 +0100

    feat: parametrizable select examples

--- thinkgpt/gpt_select.py
+++ thinkgpt/gpt_select.py
@@ -56,10 +56,10 @@ class SelectChain(LLMChain):
     @classmethod
     def from_llm(cls, llm: BaseLLM, select_examples: Optional[List] = None, verbose: bool = True) -> LLMChain:
         SELECT_PROMPT = FewShotPromptTemplate(
-            prefix="Choose the correct answer for the following question from the provided options. {instruction_hint}",
+            prefix="Choose the correct answer for the following question from the provided options.",
             examples=select_examples or SELECT_EXAMPLES,
             example_prompt=SELECT_EXAMPLE_PROMPT,
-            suffix="Question:\n{question}\nOptions:\n{options_text}\nAnswer:\n",
+            suffix="{instruction_hint}\nQuestion:\n{question}\nOptions:\n{options_text}\nAnswer:\n",
             input_variables=["instruction_hint", "question", "options_text"]
         )
         return cls(prompt=SELECT_PROMPT, llm=llm, verbose=verbose)
