7118519324ac7db564fc1537c1aeb5ed720c433d
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-04-30 13:32:53 +0200

    feat: add max tokens parameter

--- thinkgpt/memory.py
+++ thinkgpt/memory.py
@@ -1,5 +1,5 @@
 import re
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Optional
 
 import numpy as np
 from langchain import PromptTemplate, LLMChain
@@ -7,6 +7,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import OpenAI, BaseLLM
 from docarray import Document, DocumentArray
 
+from thinkgpt.helper import get_n_tokens, fit_context
 
 EXECUTE_WITH_CONTEXT_PROMPT = PromptTemplate(template="""
 Given a context information, reply to the provided request
@@ -42,7 +43,7 @@ class MemoryMixin:
         else:
             raise ValueError('wrong type, must be either str, Document, DocumentArray, List')
 
-    def remember(self, concept: Union[str, Document] = None, limit: int = 5, sort_by_order: bool = False) -> List[str]:
+    def remember(self, concept: Union[str, Document] = None, limit: int = 5, sort_by_order: bool = False, max_tokens: Optional[int] = None) -> List[str]:
         if len(self.memory) == 0:
             return []
         if concept is None:
@@ -59,4 +60,7 @@ class MemoryMixin:
         # memory needs to be sorted in chronological order
         if sort_by_order:
             docs = sorted(docs, key=lambda doc: doc.tags['mem_cnt'])
-        return [doc.text for doc in docs]
+        text_results = [doc.text for doc in docs]
+        if max_tokens:
+            text_results = fit_context(text_results, max_tokens)
+        return text_results
